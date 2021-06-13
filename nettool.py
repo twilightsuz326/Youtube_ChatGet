2b0d6b6fb2ecc448bc2c868a97f3834bdf71066b
twilightsuz326 <twilight.suz326@gmail.com>
2021-06-13 16:49:27 +0900

    LINE送信・通貨追加・Colabでも動作

--- nettool.py
+++ nettool.py
@@ -1,5 +1,6 @@
 import httplib2
 import urllib
+import urllib.request
 import csv
 
 http = httplib2.Http()
@@ -16,3 +17,23 @@ def phpapi(body):
         body=urllib.parse.urlencode(body) )[1].decode("utf-8")
 
     return content.splitlines()
+
+def sendline(message):
+    LINE_NOTIFY_URL = 'https://notify-api.line.me/api/notify'
+    TOKEN = "RX2ZD1aGdtOhVWQwsJBowhvGtAbErW0f8pivQsgRB8m"
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization': 'Bearer ' + TOKEN
+    }
+    body = {
+        'message': message,
+    }
+    data = urllib.parse.urlencode(body)
+
+    req = urllib.request.Request(LINE_NOTIFY_URL, data=data.encode('utf-8'), method='POST', headers=headers)
+    with urllib.request.urlopen(req) as res:
+        print(res.read())
+        response_body = res.read().decode("utf-8")
+
+if __name__ == "__main__":
+    sendline("a")
--- toyen.py
+++ toyen.py
@@ -1,14 +1,19 @@
 import re
+import nettool
+import sys
 
 Rate = {
+    "¥": 1,
     "￥": 1, 
+    "\\": 1, 
     "₩": 0.1, # ウォン
     "£": 154, # ポンド
     "$": 108.97, # USD
     "A$": 108.97, # USD 
-    "R$": 7.73, # レアル (ブラジル)
-    "€": 132, # ユーロ
+    "R$": 21.6, # レアル (ブラジル)
+    "€": 132, # ユーロ 
     "₹": 1.49, # ルピー (インド)
+    "₱": 2.28, # ペソ(フィリピン) (PHP)
     "NT$": 3.89, # ニュー台湾ドル
     "NZ$": 78.0, # ニュー台湾ドル
     "HK$": 14.03, #香港ドル
@@ -16,7 +21,7 @@ Rate = {
     "MX$": 5.47, # ペソ （メキシコ)
     "CHF ": 120.88, # フラン (スイス)
     "HUF ": 0.38, # フォリント（ハンガリー)
-    "ARS ": 1.16, # ペソ (アルゼンチン)
+    "ARS ": 1.154, # ペソ (アルゼンチン)
     "PHP ": 2.28, # ペソ (フィリピン)
     "COP ": 0.03, # ペソ (コロンビア)
     "CLP ": 0.15, # ペソ (チリ)
@@ -33,6 +38,22 @@ Rate = {
     "ZAR ": 8.04, # ランド (南アフリカ)
     "PEN ": 28.44, # ペルーソル
     "ISK ": 0.91, # アイスランド
+    "AED ": 29.78, # ディルハム(アラブ)
+    "JOD ": 154.30, # ヨルdan
+    "SAR ": 29.16, # riyaru saujiarabia
+    "GTQ ": 14.17, # クツアル(guatemara)
+    "RON ": 27.08, # レウ(ルーマニア)
+    "BYN ": 43.90, # beraru-si(ru-buru)
+    "TRY ": 12.98, # トルコリラ
+    "BOB ": 15.89, # boribia-no
+    "HRK ": 17.79, # クーナ クロアチア
+    "DOP ": 1.92, # peso dominika
+    "UYU ": 2.51, # peso uruguai
+    "HNL ": 4.56, # renpira honjurasu
+    "NIO ": 3.10, # nikaragua korudoba
+    "₪": 33.72,
+    "RSD ": 1.13, #ディナール (セルビア)
+    "BGN ": 67.95, # レフ(ブルガリア)
 }
 
 def toyen(paid):
@@ -40,14 +61,18 @@ def toyen(paid):
     try:
         return int(float(yeng.group(2)) * Rate[yeng.group(1)])
     except:
+        import traceback
         print("★ERROR!★")
+        nettool.sendline("ERROR: {}".format(paid))
+        nettool.sendline(traceback.print_exc())
+        print("")
         return 0
 
 def toPaidGroup(paid):
     paid = paid.replace(",", "")
-    regit = re.compile("(.*\s|\$|.*\$|￥|₩|£|€|₹)(.*)")
+    regit = re.compile("(.*\s|\$|.*\$|¥|￥|₩|£|€|₹|₪|₱)(.*)")
     one_int = regit.match(paid)
     return one_int
 
 if __name__ == '__main__':
-    print(toyen("$100.00"))
+    print(toyen("¥100"))
